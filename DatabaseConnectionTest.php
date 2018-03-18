f9d9f528667cd54fde493d98f132e65ad93ce46c
Mickey P <anamika.pancholy@gmail.com>
2018-03-18 05:45:37 -0400

    Changed table queried in Connection Test file.

--- DatabaseConnectionTest.php
+++ DatabaseConnectionTest.php
@@ -10,7 +10,7 @@
             $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$PDOresults = $connection->query("select * from students;");
+		$PDOresults = $connection->query("select * from employees;");
 
 		while($row = $PDOresults->fetch(PDO::FETCH_ASSOC)){
 			foreach($row as $column){
