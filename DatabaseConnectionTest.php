a0bf7e4ee9d7bdfac3263474e7f2baafb396ca77
Mickey P <anamika.pancholy@gmail.com>
2018-03-18 05:46:55 -0400

    Fixed indentation in Connection Test file.

--- DatabaseConnectionTest.php
+++ DatabaseConnectionTest.php
@@ -1,21 +1,21 @@
 
 <?php
 	$servername = "ryc353.encs.concordia.ca";
-        $dbname = "ryc353_4";
-        $username = "ryc353_4";
-        $password = "353winte";
+	$dbname = "ryc353_4";
+	$username = "ryc353_4";
+	$password = "353winte";
 
-        #Using PDO
+	#Using PDO
 
-            $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$connection = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$PDOresults = $connection->query("select * from employees;");
+	$PDOresults = $connection->query("select * from employees;");
 
-		while($row = $PDOresults->fetch(PDO::FETCH_ASSOC)){
-			foreach($row as $column){
-				echo $column." ";
-			}
-			echo'<br/>';
+	while($row = $PDOresults->fetch(PDO::FETCH_ASSOC)){
+		foreach($row as $column){
+			echo $column." ";
 		}
+		echo'<br/>';
+	}
 ?>
