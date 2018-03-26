dcd3da396b2babefa7ae362af2ae89451f7e5583
HagopA <hagopavakian@hotmail.com>
2018-03-26 17:15:57 -0400

    Added drop down and labels

--- add.php
+++ add.php
@@ -2,7 +2,6 @@
 			include("COMP353-Main/DatabaseMethods.php");
 			include("COMP353-Main/frontEnd.php");
 		?>
-
 		<form id="form" method="post">
 			<label><input type = "radio" name="table" value="Employee"/> Employee</label></br>
 			<label><input type = "radio" name="table" value="Dependents"/> Dependent</label></br>
@@ -11,23 +10,77 @@
 			<label><input type = "radio" name="table" value="DepartmentLocation"/> Department Location </label></br>
 			<label><input type = "radio" name="table" value="Project"/> Project </label></br>
       <label><input type = "radio" name="table" value="WorksOn"/> Works On </label></br>
-			<input type="submit" value="Submit">
-
 		</form>
 		<script>
-      $("document").ready(function() {
-        $( "input[type=radio][name=table]" ).change(function(){
-                                           $("form").submit();
+      $(document).ready(function() {
+        $('input[type=radio][name=table]').change(function(){
+                                           $('form').submit();
                                            });
       });
+      
+      //SIN, Name, DateOfBirth, Gender, Address, PhoneNumber, 
+//Salary, SupervisorId, DepartmentNumber
 	</script>
 		<?php
 			if(isset($_POST['table'])){
+        $PDOresults;
+        echo "<form>";
         switch($_POST['table']){
           case "Employee":
-        			echo "test1"; break;
+        	echo "SIN: <input type='number' name='SIN'/></br>";
+            	echo "Name: <input type='text' name='name'/></br>";
+            	echo "Date Of Birth: <input type='date' name='dateofbirth'/></br>";
+            	echo "Gender: <input type='text' name='gender'/></br>";
+            	echo "Employee: <input type='radio' name='employee'/></br>";
+            	echo "Address: <input type='text' name='address'/></br>";
+            	echo "Phone Number: <input type='number' name='PhoneNumber'/></br>";
+            	echo "Salary: <input type='number' name='salary'/></br>";
+		$PDOresults = getEmployeesKeys();
+            	echo "<select name='supervisorID'>";
+          		
+            	while($PDOresults != NULL && $row = $PDOresults->fetch(PDO::FETCH_ASSOC))
+							{
+								foreach($row as $column)
+								{
+									echo "<option value=$column> $column</option>";
+								}
+							}
+            	echo "</select>";
+            	$PDOresults = getDepartmentsKeys();
+
+            	echo "<select name='DepartmentNumber'>";
+           		while($PDOresults != NULL && $row = $PDOresults->fetch(PDO::FETCH_ASSOC))
+							{
+								foreach($row as $column)
+								{
+									echo "<option value=$column> $column</option>";
+								}
+							}
+            	echo "</select>";
+            break;
+            
+            
+            case "Dependents":
+        			echo "<form><input type='number' name='SIN'/></br>";
+            break;
+            case "Employee":
+        			echo "<form><input type='number' name='SIN'/></br>";
+            break;
+            case "Employee":
+        			echo "<form><input type='number' name='SIN'/></br>";
+            break;
+            case "Employee":
+        			echo "<form><input type='number' name='SIN'/></br>";
+            break;
+            case "Employee":
+        			echo "<form><input type='number' name='SIN'/></br>";
+            break;
+            case "Employee":
+        			echo "<form><input type='number' name='SIN'/></br>";
+            break;
 	
           default: echo "test2";
+          echo "</form>";
 }
       }
 		?>
